23438063ecb8ee72687e5dea2e5f155b42131403
hyoeun9931 <leehyoeun7454@gmail.com>
2026-04-16 17:21:38 +0900

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import os
 import streamlit as st
-
+from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
